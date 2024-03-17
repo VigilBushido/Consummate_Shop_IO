6179eadcdb188058dcda552a6f6f6d952e6a574f
VigilBushido <vigilbushido@gmail.com>
2024-03-17 05:16:19 -0400

    Header Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <h1>Welcome to the Consummate Shop IO</h1>
-  )
-}
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to the Consummate Shop IO</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App
