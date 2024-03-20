11e167c6b0cbad02e3be9d3f021ed50f620f31df
VigilBushido <vigilbushido@gmail.com>
2024-03-20 04:06:21 -0400

    ProductScreen

---
+++ frontend/src/screens/ProdcutScreen.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import products from "../products";
+
+const ProdcutScreen = () => {
+    const { id: productId } = useParams();
+    const product = products.find((p) => p._id === productId);
+    console.log(product);
+    
+  return (
+    <div>ProdcutScreen</div>
+  )
+}
+export default ProdcutScreen
