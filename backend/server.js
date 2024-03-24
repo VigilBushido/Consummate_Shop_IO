9381f4232777a75deaf08d60f3852ee837910132
VigilBushido <vigilbushido@gmail.com>
2024-03-24 10:24:11 -0400

    seeder sample data + models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
-
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
