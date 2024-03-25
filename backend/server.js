058afca0f257f872bf8d31635828185528e5f41c
VigilBushido <vigilbushido@gmail.com>
2024-03-25 05:45:50 -0400

    cleaned up routes + custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
@@ -14,5 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
