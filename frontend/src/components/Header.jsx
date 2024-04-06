53f5a93d30f953aad3cc496318f165288b2d0cef
VigilBushido <vigilbushido@gmail.com>
2024-04-05 23:06:01 -0400

    + cart items hook

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+    //console.log(cartItems);
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -18,7 +22,15 @@ const Header = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <LinkContainer to="/cart">
-                                <Nav.Link><FaShoppingCart /> Cart</Nav.Link></LinkContainer>
+                                <Nav.Link><FaShoppingCart /> Cart
+                                    {
+                                        cartItems.length > 0 && (
+                                            <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                                {cartItems.reduce((acc, cur_item) => acc + cur_item.qty, 0)}
+                                            </Badge>
+                                        )
+                                    }
+                                </Nav.Link></LinkContainer>
                             <LinkContainer to="/login">
                                 <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link></LinkContainer>
                         </Nav>
