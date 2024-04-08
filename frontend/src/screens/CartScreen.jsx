f578edb6919443ea86ea12e4d6c797f55a38d0c2
VigilBushido <vigilbushido@gmail.com>
2024-04-07 23:29:42 -0400

    + subtotal card

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -56,6 +56,23 @@ const CartScreen = () => {
                 </ListGroup>
             )}
         </Col>
+        <Col md={4}>
+            <Card>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>
+                            Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
+                        </h2>
+                        ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Button type='button' className='btn-block' disabled={cartItems.Length === 0}>
+                            Proceed To Checkout
+                        </Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </Col>
     </Row>;
 };
 export default CartScreen;
