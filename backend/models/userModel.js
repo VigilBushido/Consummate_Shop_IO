0aefb738fbed14deb938ed9387c35a090a56a0ec
VigilBushido <vigilbushido@gmail.com>
2024-05-07 00:31:59 -0400

    dry- register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+    // if dealing with user data and this is not modified it will continue
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
