360735952a1319a697bf3893bfad99a50e143143
VigilBushido <vigilbushido@gmail.com>
2024-05-09 23:47:01 -0400

    + get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile user');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Update user profile
