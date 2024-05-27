61e42edeb67c726c2bd772da3995fa742480cdf8
VigilBushido <vigilbushido@gmail.com>
2024-05-26 23:02:48 -0400

    +Form Group & useState

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,4 +1,6 @@
 import FormContainer from '../components/FormContainer';
+import { Form, Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 const ShippingScreen = () => {
     const [address, setAddress] = useState('');
@@ -6,6 +8,21 @@ const ShippingScreen = () => {
     const [postalCode, setPostalCode] = useState('');
     const [country, setCountry] = useState('');
 
-    return <div>shippingScreen</div>;
+    const submitHandler = (e) => {
+        e.prevenDefault();
+        console.log('submit');
+    };
+
+    return <FormContainer>
+        <h1>Shipping</h1>
+
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='address' className='my-2'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control type="text" placeholder="Enter address" value={address} onChange={(e) => setAddress(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+        </Form>
+    </FormContainer>;
 };
 export default ShippingScreen;
