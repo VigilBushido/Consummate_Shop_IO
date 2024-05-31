0c123d321ce98ced755e19d6bc39b9c4f51c30e8
VigilBushido <vigilbushido@gmail.com>
2024-05-30 23:23:30 -0400

    + private routes impl

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import App from './App';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -28,7 +29,11 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
+
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
