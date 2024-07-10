da7dbb0ac598b90b0398cf9930b904c670acc190
VigilBushido <vigilbushido@gmail.com>
2024-07-09 21:52:40 -0400

    +Delivered  / Not Delivered

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -7,13 +7,44 @@ import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice';
 const OrderScreen = () => {
     const { id: orderId } = useParams();
 
-    const { data: order, refetch, isLoading, isError } =
+    const { data: order, refetch, isLoading, error } =
         useGetOrderDetailsQuery(orderId);
 
     console.log(order);
 
-    return (
-        <div>OrderScreen</div>
+    return isLoading ? <Loader /> : error ? <Message variant='danger' /> : (
+        <>
+            <h1>Order {order._id}</h1>
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p>
+                                <strong>Name: </strong> {order.user.name}
+                            </p>
+                            <p>
+                                <strong>Email: </strong> {order.user.email}
+                            </p>
+                            <p>
+                                <strong>Address: </strong>
+                                {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
+                                {order.shippingAddress.postalCode},{' '}
+                                {order.shippingAddress.country}
+                            </p>
+                            {order.isDelivered ? (
+                                <Message variant='success'>
+                                    Delivered on {order.deliveredAt}
+                                </Message>
+                            ) : (
+                                <Message variant='danger'>Not Delivered</Message>
+                            )}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={4}>Column</Col>
+            </Row>
+        </>
     );
 };
-export default OrderScreen;
+export default OrderScreen; 
