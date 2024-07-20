9c9561e2a8124d0c18a38ef5156a9d7739091e44
VigilBushido <vigilbushido@gmail.com>
2024-07-19 23:50:16 -0400

    updatePaid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -70,7 +70,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     if (order) {
         order.isPaid = true;
         order.paidAt = Date.now();
-        order.payentResult = {
+        order.paymentResult = {
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
