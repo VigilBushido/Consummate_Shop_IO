6059fb55fe7017a614974990e701265892b94235
VigilBushido <vigilbushido@gmail.com>
2024-12-17 23:19:22 -0500

    admin + orderlist

--- frontend/src/screens/Admin/OrderListScreen.jsx
+++ frontend/src/screens/Admin/OrderListScreen.jsx
@@ -3,14 +3,35 @@ import { Table, Button } from 'react-bootstrap';
 import { FaTimes } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useGetOrdersQuery } from '../../slices/ordersApiSlice/';
+import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
 
 const OrderListScreen = () => {
     const { data: orders, isLoading, error } = useGetOrdersQuery();
-    console.log(orders);
+    //console.log(orders);
 
     return (
-        <div>OrderListScreen</div>
+        <>
+            <h1>Orders</h1>
+            {isLoading ? (<Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : (
+                <Table striped hover responsive
+                    className='table-sm'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>USER</th>
+                            <th>DATE</th>
+                            <th>TOTAL</th>
+                            <th>PAID</th>
+                            <th>DELIVERED</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </Table>
+            )}
+        </>
     );
 };
 export default OrderListScreen;
