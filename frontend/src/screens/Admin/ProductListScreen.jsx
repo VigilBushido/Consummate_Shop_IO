b3d2972fa4c5bd527f45f6681baaa2a284d017ec
VigilBushido <vigilbushido@gmail.com>
2025-01-31 21:07:20 -0500

    useGetProductsQuery

--- frontend/src/screens/Admin/ProductListScreen.jsx
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -1,10 +1,15 @@
 import { LinkContainer } from 'react-router-bootstrap';
-import { Table, Button, Row, Column } from 'react-bootstrap';
-import { FaTimes } from 'react-icons/fa';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
 
 const ProductListScreen = () => {
+    const { data: product, isLoading, error } = useGetProductsQuery();
+
+    console.log(product);
+
     return (
         <div>ProductListScreen</div>
     );
