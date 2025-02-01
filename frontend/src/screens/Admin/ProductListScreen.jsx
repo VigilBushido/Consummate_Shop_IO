0a749cfd96d8a9c86b7c6676d45f4758a1f1f5f3
VigilBushido <vigilbushido@gmail.com>
2025-01-31 21:00:45 -0500

    impl admin product list

---
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -0,0 +1,12 @@
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Column } from 'react-bootstrap';
+import { FaTimes } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+
+const ProductListScreen = () => {
+    return (
+        <div>ProductListScreen</div>
+    );
+};
+export default ProductListScreen;
