e5a76cf9824aef0a133dfc0f02c54a45a2234092
VigilBushido <vigilbushido@gmail.com>
2025-06-06 23:53:23 -0400

    imports

--- frontend/src/screens/Admin/ProductEditScreen.jsx
+++ frontend/src/screens/Admin/ProductEditScreen.jsx
@@ -1,3 +1,12 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import { toast } from "react-toastify";
+import { useUpdateProductMutation } from "../../slices/productsApiSlice";
+
 const ProductEditScreen = () => {
     return (
         <div>ProductEditScreen</div>
